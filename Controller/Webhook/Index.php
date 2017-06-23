1c235f9fafaffcf5cda9232267956ee2cf3d0a65
Laurent Clouet <laurent35240@gmail.com>
2017-06-23 15:18:45 +0200

    Webhook secret in back office

---
+++ Controller/Webhook/Index.php
@@ -0,0 +1,23 @@
+<?php
+
+
+namespace Laurent35240\GoCardless\Controller\Webhook;
+
+
+use Magento\Framework\App\Action\Action;
+use Magento\Framework\App\ResponseInterface;
+
+class Index extends Action
+{
+
+    /**
+     * Dispatch request
+     *
+     * @return \Magento\Framework\Controller\ResultInterface|ResponseInterface
+     * @throws \Magento\Framework\Exception\NotFoundException
+     */
+    public function execute()
+    {
+        // TODO: Implement execute() method.
+    }
+}
